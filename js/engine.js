6389123440bb191a8329cfc2aaf621301b09abe8
Adam Wolski <wolas.abc@gmail.com>
2013-09-03 12:53:36 +0200

    renderer object + stats.js

--- js/engine.js
+++ js/engine.js
@@ -1,1 +1,72 @@
 var ENGINE = ENGINE || {};
+
+//renderer 
+
+ENGINE.Renderer = function( opts ) {
+	//params
+	opts = opts || {};
+	//vars
+
+	//internal vars
+	var _gl, _canvas;
+
+	_canvas = document.getElementById('canvas');
+
+	this.getContext = function() {
+		return _gl;
+	}
+
+	this.setViewport = function ( x, y, width, height ) {
+
+		_viewportX = x !== undefined ? x : 0;
+		_viewportY = y !== undefined ? y : 0;
+
+		_viewportWidth = width !== undefined ? width : _canvas.width;
+		_viewportHeight = height !== undefined ? height : _canvas.height;
+
+		_gl.viewport( _viewportX, _viewportY, _viewportWidth, _viewportHeight );
+
+	};
+
+	this.setSize = function(width, height) {
+		width = parseInt(width, 10);
+		height = parseInt(height, 10);
+		_canvas.width = width;
+		_canvas.height = height;
+		_canvas.style.width = width + 'px';
+		_canvas.style.height = height + 'px';
+
+		this.setViewport(0, 0, _canvas.width, _canvas.height);
+	}
+
+	this.render  = function ( scene, camera, renderTarget, forceClear ) {
+
+	}
+
+	//internal functions
+
+	function create3DContext(canvas, opt_attribs) {
+	  var names = ["webgl", "experimental-webgl", "webkit-3d", "moz-webgl"];
+	  var context = null;
+	  for (var ii = 0; ii < names.length; ++ii) {
+	    try {
+	      context = canvas.getContext(names[ii], opt_attribs);
+	    } catch(e) {}
+	    if (context) {
+	      break;
+	    }
+	  }
+	  return context;
+	}
+
+	function initGL() {
+	 	_gl = create3DContext(_canvas);
+	}
+
+	initGL();
+
+};
+
+ENGINE.Renderer.prototype = {
+	constructor: ENGINE.WebGLRenderer
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,48 @@
-$(function(){
+var ROLLERCOASTER = (function() {
+	//private vars
+	var _renderer, _stats = false;
+
+
+	var onWindowResize = function() {
+		_renderer.setSize( window.innerWidth, window.innerHeight );
+	},
+	prepareEvents = function() {
+		$(window).resize(onWindowResize);
+	},
+	init = function(opts) {
+		opts = opts || {};
+		opts.stats = opts.stats || true;
+
+		generateScene();
+		prepareEvents();
+		animate();
+		if(opts.stats) showStats();
+	},
+	generateScene = function() {
+		_renderer = new ENGINE.Renderer();
+        _renderer.setSize( window.innerWidth, window.innerHeight );
+	},
+	animate = function () {
+		requestAnimationFrame( animate );
+		render();
+		if(_stats) _stats.update();
+	},
+	render = function() {
+		_renderer.render();
+	},
+	showStats = function() {
+		_stats = new Stats();
+		_stats.setMode(1); // 0: fps, 1: ms
+
+		// Align bottom-left
+		_stats.domElement.style.position = 'absolute';
+		_stats.domElement.style.left = '0px';
+		_stats.domElement.style.bottom = '0px';
+
+		document.body.appendChild( _stats.domElement );
+	};
 	
-});
+	return {
+		start : init
+	}
+})();
